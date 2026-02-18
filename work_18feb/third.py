67ebc33b4878ed39fa67f9dcedf674627766f5fe
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-18 23:56:18 +0530

    this file contain third task program

---
+++ work_18feb/third.py
@@ -0,0 +1,6 @@
+total_student=125
+student_class=30
+full_class=total_student//student_class
+remaining_student=total_student%student_class
+print("Full classes:",full_class)
+print("Students without class:",remaining_student)
