7ff5fc6e389f8a2e3cd4ac3be8a6ed75c74cde9c
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-21 23:28:47 +0530

    this file contain hollow pyramid pattern program

---
+++ self_learning/pattern_program_20feb/hollow pyramid pattern.py
@@ -0,0 +1,23 @@
+n = 5
+for i in range(n):
+    print(" " * (n-i-1), end="")
+    for j in range(2*i+1):
+        if j == 0 or j == 2*i or i == n-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+
+
+
+
+
+
+'''Output:
+    *
+   * *
+  *   *
+ *     *
+*********
+    ''' 
