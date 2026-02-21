a8a5955991f1c28f75f240d4622df38181297eec
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-21 23:15:34 +0530

    this file contain box pattern with row index pattern program

---
+++ self_learning/pattern_program_20feb/box pattern with row index.py
@@ -0,0 +1,21 @@
+n = 5
+for i in range(n):
+    for j in range(n):
+        print(i+1, end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+    '''    Output:
+    1 1 1 1 1
+    2 2 2 2 2
+    3 3 3 3 3
+    4 4 4 4 4
+    5 5 5 5 5
+    '''
