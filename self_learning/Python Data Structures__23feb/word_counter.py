c26d738cca70e86f78546a497c4864df93786c3a
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 01:26:43 +0530

    this file contain count words in sentence program

---
+++ self_learning/Python Data Structures__23feb/word_counter.py
@@ -0,0 +1,15 @@
+# Program to count number of words in a sentence
+
+sentence = input("Enter a sentence: ")
+
+# Split sentence into words
+words = sentence.split()
+
+print("Total number of words:", len(words))
+   
+
+#-------------------------------------------
+'''#output
+Enter a sentence: Hello world 123
+Total number of words: 3
+'''
