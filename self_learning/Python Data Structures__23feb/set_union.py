b1ad0e5c5f287e7e3f1641261075e4da128224da
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 01:37:02 +0530

    this file contain union of two sets program

---
+++ self_learning/Python Data Structures__23feb/set_union.py
@@ -0,0 +1,17 @@
+# Program to find union of two sets
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+# Union combines all unique elements
+result = set1.union(set2)
+
+print("Union of sets:", result)
+
+
+
+
+#-------------------------------------------------
+'''#output
+Union of sets: {1, 2, 3, 4, 5}
+'''
