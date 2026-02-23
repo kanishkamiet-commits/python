e0cb67ee15cad09e7cf82caea24ee70a1a47805c
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 00:12:10 +0530

    this  file contain string methods demo program

---
+++ work_23feb/class_work/Numeric_string .py
@@ -0,0 +1,21 @@
+#isalpha() method returns True if all the characters are alphabet letters (a-z) and there is at least one character, otherwise it returns False.``
+sentence="Hello world"
+print(sentence.isalpha())
+
+
+#isalnum() method returns True if all the characters are alphanumeric (a-z, A-Z, 0-9) and there is at least one character, otherwise it returns False.
+str3='hello 123'
+print(str3.isalnum())
+
+
+#isalnum() method returns True if all the characters are alphanumeric (a-z, A-Z, 0-9) and there is at least one character, otherwise it returns False.
+str4="123"
+print(str4.isalnum())
+
+
+
+
+'''#output
+False
+False
+True'''
