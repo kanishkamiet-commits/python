65c7b6f02e85b55a8d72357c0d877c255bfea9fe
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 11:06:52 +0530

    this file contain even  odd without modulus program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/even_odd_without_modulus.py
@@ -0,0 +1,28 @@
+# -------------------------------------------------
+# Program: Check Even or Odd
+#          (Without using modulus operator)
+# ------------------------------------------------
+
+#enter an integer 
+num = int(input("Enter an integer: "))
+
+
+    # Check even or odd without %
+if num // 2 * 2 == num:
+        print("The number is Even.")
+else:
+        print("The number is Odd.")
+
+
+
+
+
+
+# -------------------------------------------------
+'''#output
+Enter an integer: 5
+The number is Odd.
+#----------------------------------------------
+Enter an integer: 10
+The number is Even.
+'''
