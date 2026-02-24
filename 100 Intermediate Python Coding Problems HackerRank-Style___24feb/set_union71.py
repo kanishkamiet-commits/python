1eecaf3f0d6b9b5a90da2b56f86bdd908875fad5
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:15:29 +0530

    this file contain set_union program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/set_union71.py
@@ -0,0 +1,16 @@
+#input two sets and find their union
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+union_set = set1.union(set2)
+#display the sets and their union
+print(f"Set 1: {set1}")
+print(f"Set 2: {set2}")
+#display the union of the two sets
+print(f"Union: {union_set}")
+
+'''
+Output:
+Set 1: {1, 2, 3, 4, 5}
+Set 2: {4, 5, 6, 7, 8}
+Union: {1, 2, 3, 4, 5, 6, 7, 8}
+'''
