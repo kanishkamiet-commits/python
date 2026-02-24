bc10258666b75388dfd4af3f349729272110d2f6
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:00:08 +0530

    this file contain sum of list elements program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/sum_of_list_elements62.py
@@ -0,0 +1,12 @@
+#input a list of numbers and calculate the sum of all elements in the list
+numbers = [1, 2, 3, 4, 5]
+total_sum = sum(numbers)
+#print the list and the sum of all elements
+print(f"List: {numbers}")
+print(f"Sum of all elements: {total_sum}")
+
+'''
+Output:
+List: [1, 2, 3, 4, 5]
+Sum of all elements: 15
+'''
